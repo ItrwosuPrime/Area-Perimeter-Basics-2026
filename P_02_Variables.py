a8267abd225e6fb7f57f4c31a02dacbbf699f93f
ItrwosuPrime <defontenayl50013@masseyhigh.school.nz>
2026-03-20 10:47:11 +1300

    Variable Basics

---
+++ P_02_Variables.py
@@ -0,0 +1,11 @@
+# Variable code lesson
+
+# Variables can be a string (speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are UPPERCASE
+PI = 3.14
